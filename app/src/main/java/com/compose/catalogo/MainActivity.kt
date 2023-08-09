f0bd87590b4eb8dcfb0862f2a9422b1baefb0f6f
Cristian Fuentes <cristian.restrepo@mercadolibre.com.co>
2023-08-09 14:51:37 -0500

    (Progress): Section 2

--- app/src/main/java/com/compose/catalogo/MainActivity.kt
+++ app/src/main/java/com/compose/catalogo/MainActivity.kt
@@ -3,13 +3,24 @@ package com.compose.catalogo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.compose.catalogo.ui.theme.CatalogoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +30,9 @@ class MainActivity : ComponentActivity() {
             CatalogoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyComplexLayout()
                 }
             }
         }
@@ -30,17 +40,72 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyComplexLayout() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(Color.Blue),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Ejemplo tres")
+        }
+
+        MySpacer(30)
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            Box(
+                modifier = Modifier
+                    .background(Color.Gray)
+                    .weight(1f)
+                    .fillMaxHeight(),
+                Alignment.Center
+            ) {
+                Text(text = "Ejemplo dos")
+            }
+            Box(
+                modifier = Modifier
+                    .background(Color.Green)
+                    .weight(1f)
+                    .fillMaxHeight(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Soy Cristian Fuentes")
+            }
+        }
+
+        MySpacer(5)
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(Color.Red),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Text(text = "Ejemplo cuatro", color = Color.White)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CatalogoTheme {
-        Greeting("Android")
+        MyComplexLayout()
     }
+}
+
+@Composable
+fun MySpacer(size: Int) {
+    Spacer(
+        modifier = Modifier
+            .height(size.dp)
+            .fillMaxWidth()
+    )
 }
