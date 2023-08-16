631413d815f3766b5865042c46b5169ad1ba805a
Cristian Fuentes <cristian.restrepo@mercadolibre.com.co>
2023-08-16 16:45:49 -0500

    (Pregress): Section 3

--- app/src/main/java/com/compose/catalogo/MainActivity.kt
+++ app/src/main/java/com/compose/catalogo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,10 +13,15 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -32,10 +38,34 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    MyComplexLayout()
+                    MyStateExample()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MyStateExample() {
+
+    var counter by rememberSaveable { mutableStateOf(0) }
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { counter += 1 }) {
+            Text(text = "Pulsar")
+        }
+        Text(text = "He sido pulsado $counter veces")
+    }
+}
+
+@Preview(showBackground = true, name = "Name preview")
+@Composable
+fun GreetingPreview() {
+    CatalogoTheme {
+        MyStateExample()
     }
 }
 
@@ -90,14 +120,6 @@ fun MyComplexLayout() {
         ) {
             Text(text = "Ejemplo cuatro", color = Color.White)
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CatalogoTheme {
-        MyComplexLayout()
     }
 }
 
